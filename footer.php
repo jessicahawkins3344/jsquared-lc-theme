bab64d78eb1f285f12fd00a1db44cdd77dbd86e3
Jessica Hawkins <thehawk@jhawks-mb-air.attlocal.net>
2017-02-15 16:53:51 -0600

    fix countimator

--- footer.php
+++ footer.php
@@ -2,12 +2,5 @@
 	<?php echo dslc_hf_get_footer(); ?>
 
 	<?php wp_footer(); ?>
-<script>
-   jQuery(document).ready(function( $ ) {
-  $(".counter").countimator({
-		   	duration: 2400
-		});
-});
-</script>
 </body>
 </html>
